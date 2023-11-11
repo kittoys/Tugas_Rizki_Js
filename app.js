d3c5d9569b502068e5c97c81a458dc7bca637c48
Rizki ramadhan <115064027+kittoys@users.noreply.github.com>
2023-11-11 20:18:06 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+const module1 = require('./module1');
+const module2 = require('./module2');
+const module3 = require('./module3');
+const module4 = require('./module4');
+
+module1.greet("Rizki Ramadan");
+module2.calculate(6, 12);
+module3.sayHello("Nanda");
+module4.double(128);
